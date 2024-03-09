c152578f3666c2eef05f7c5aea018e08d3b92bff
markusp00 <markus.pedersen.00@gmail.com>
2024-03-09 13:18:42 +0100

    WIP

--- Markus/test_script.m
+++ Markus/test_script.m
@@ -3,13 +3,25 @@ Message = "Hello world";
 MessageLength = strlength(Message) + 5;
 SamplesPerSymbol = 12;
 
+% Setup Receiver
+rx = sdrrx('Pluto','OutputDataType','double','SamplesPerFrame',2^15);
+rx.CenterFrequency = 916e6;
+rx.BasebandSampleRate = 400000;
+rx.SamplesPerFrame = 11226;
+% Setup Transmitter
+tx = sdrtx('Pluto','Gain',-30);
+tx.CenterFrequency = 916e6;
+tx.Gain = 0;
+tx.BasebandSampleRate = 400000;
+rx.SamplesPerFrame = 11226;
+
 % Constellation diagrams
 constDiagram1 = comm.ConstellationDiagram('SamplesPerSymbol',SamplesPerSymbol, ...
-    'SymbolsToDisplaySource','Property','SymbolsToDisplay',100);
+    'SymbolsToDisplaySource','Property','SymbolsToDisplay',100,'Title','txData');
 constDiagram2 = comm.ConstellationDiagram('SamplesPerSymbol',SamplesPerSymbol, ...
-    'SymbolsToDisplaySource','Property','SymbolsToDisplay',100);
+    'SymbolsToDisplaySource','Property','SymbolsToDisplay',100,'Title','filteredData');
 constDiagram3 = comm.ConstellationDiagram('SamplesPerSymbol',SamplesPerSymbol, ...
-    'SymbolsToDisplaySource','Property','SymbolsToDisplay',100);
+    'SymbolsToDisplaySource','Property','SymbolsToDisplay',100,'Title','agcData');
 constDiagram4 = comm.ConstellationDiagram('SamplesPerSymbol',SamplesPerSymbol, ...
     'SymbolsToDisplaySource','Property','SymbolsToDisplay',100);
 constDiagram5 = comm.ConstellationDiagram('SamplesPerSymbol',SamplesPerSymbol, ...
@@ -29,11 +41,19 @@ coarseFrequencyCompensator = comm.CoarseFrequencyCompensator("Modulation","QPSK"
 symbolSynchronizer = comm.SymbolSynchronizer("Modulation","PAM/PSK/QAM","SamplesPerSymbol",SamplesPerSymbol);
 carrierSynchronizer = comm.CarrierSynchronizer("Modulation","QPSK","SamplesPerSymbol",SamplesPerSymbol);
 
-txfilter = comm.RaisedCosineTransmitFilter('OutputSamplesPerSymbol',SamplesPerSymbol);
+txfilter = comm.RaisedCosineTransmitFilter('OutputSamplesPerSymbol',SamplesPerSymbol,'RolloffFactor',0.5);
 rxfilter = comm.RaisedCosineReceiveFilter('InputSamplesPerSymbol',SamplesPerSymbol, ...
-    'DecimationFactor',12);
+    'DecimationFactor',12,RolloffFactor=0.5);
 errorRate = comm.ErrorRate('ReceiveDelay',2);
+agc = comm.AGC();
+agc.DesiredOutputPower = 2;
+agc.AveragingLength = 50;
+agc.MaxPowerGain = 60;
 
+barker = comm.BarkerCode("Length",13,SamplesPerFrame=13);
+
+
+% Barker code
 
 % Bit generation
 msgSet = zeros(100 * MessageLength, 1); 
@@ -41,19 +61,25 @@ for msgCnt = 0 : 99
     msgSet(msgCnt * MessageLength + (1 : MessageLength)) = ...
         sprintf('%s %03d\n', Message, msgCnt);
 end
-MessageBits = int2bit(msgSet, 7);
+seq = barker();
+seq
+MessageBits = [seq, int2bit(msgSet, 7)];
+
 
 % ---- Sender ----
 modSig = qpskmod(MessageBits);
 txData = txfilter(modSig);
-
+tx.transmitRepeat(txData);
 
 % ---- Channel ----
-offsetData = pfo(txData);
-rxSig = channel(offsetData);
+% offsetData = pfo(txData);
+% rxSig = channel(offsetData);
+
 
 % ---- Receiver ----
-filteredData = rxfilter(rxSig);
+
+%agcData = agc(rx());
+filteredData = rxfilter(rx());
 compensatedData = coarseFrequencyCompensator(filteredData);
 synchronizedSymbol = symbolSynchronizer(compensatedData);
 synchronizedCarrier = carrierSynchronizer(synchronizedSymbol);
@@ -62,7 +88,7 @@ rxData = qpskdemod(synchronizedCarrier);
 
 % ---- Error calculation ----
 % errorStats = errorRate(MessageBits,rxData);
-fprintf("Errors: %d\n", errorStats(1));
+% fprintf("Errors: %d\n", errorStats(1));
 
 % ---- Data decoding ----
 charSet = int8(bit2int(rxData, 1));
@@ -70,4 +96,6 @@ charSet = int8(bit2int(rxData, 1));
 
 constDiagram1(txData)
 constDiagram2(filteredData)
+%constDiagram3(agcData)
+constDiagram4(compensatedData)
 
